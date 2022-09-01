24f5986eef511bb7aab8f314d4f268cdc745cd97
Beniamin Kwak <beniaminkwak@gmail.com>
2022-09-01 07:57:46 +0200

    Add project files.

---
+++ Instytut Zadanie 1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Instytut_Zadanie_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am going to become #1 .Net Developer!");
+            Console.WriteLine("Let's start my great journey!");
+            // Dodaję ReadLine żeby Ci error nie wyskakiwał! :D
+            Console.ReadLine();
+        }
+    }
+}
